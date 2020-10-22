61a2faf9452692ec83da00e575074231de05271b
Josh Ibad <joshcibad@gmail.com>
2020-10-21 19:23:49 -0700

    Add files via upload

--- q2-joshcibad/build.sh
+++ q2-joshcibad/build.sh
@@ -1,21 +1,13 @@
-#!/bin/bash 
+#!/bin/bash
 
-#Program: "Bubble Sorting an Array of Integers" (HW 3 for CPSC 240-03, Fall 2020)
-#Author: Josh Ibad
+# Author: Josh Ibad
+# Program: CPSC 351 - Quiz 2 - Forking Processes
 
-#Delete some un-needed files
-rm *.o
-rm *.out
+echo "Removing old files if necessary"
+rm ./main.out
 
-echo "The script file for Program \"Bubble Sorting an Array of Integers\" has begun"
-
-#Compilation
-echo "Compile processes.cpp"
+echo "Compiling processes.cpp"
 g++ -o main.out processes.cpp
 
-# Run program
-echo "Run the program \"Bubble Sorting an Array of Integers\":"
-echo " "	#Blank line merely for appearances
+echo "Running program"
 ./main.out
-
-echo "The script file will terminate"
